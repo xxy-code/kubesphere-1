0973d02f0211a84b5ffee50df271bddb481b0d29
xxy-code <63041889+xxy-code@users.noreply.github.com>
2022-05-13 07:46:36 +0800

    Update options.go

--- pkg/apiserver/authentication/options.go
+++ pkg/apiserver/authentication/options.go
@@ -30,6 +30,7 @@ import (
 	_ "kubesphere.io/kubesphere/pkg/apiserver/authentication/identityprovider/github"
 	_ "kubesphere.io/kubesphere/pkg/apiserver/authentication/identityprovider/ldap"
 	_ "kubesphere.io/kubesphere/pkg/apiserver/authentication/identityprovider/oidc"
+	_ "kubesphere.io/kubesphere/pkg/apiserver/authentication/identityprovider/gitlab"
 	"kubesphere.io/kubesphere/pkg/apiserver/authentication/oauth"
 )
 
